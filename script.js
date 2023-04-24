c96390f52f71487c6deaa680cccad464e7b18f40
warnerb47 <taphagaye2001.mg@gmail.com>
2023-04-24 13:39:05 +0000

    feat: toogle menu icon

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const menu = document.querySelector('.menu');
 const navbar = document.querySelector('.navbar');
+const menuIcon = document.querySelector('.menu i');
 
 menu.addEventListener('click', () => {
     navbar.classList.toggle('open');
+    
+    if (menuIcon.classList.value.includes('fa-bars')) {
+        menuIcon.classList.remove('fa-bars');
+        menuIcon.classList.add('fa-xmark');
+    } else {
+        menuIcon.classList.add('fa-bars');
+        menuIcon.classList.remove('fa-xmark');
+    }
 });
